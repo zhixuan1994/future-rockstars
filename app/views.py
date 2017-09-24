24b055e0c775c4a2ac9e164c196a3746a7d17fff
Sean Collins <sean.collins@outlook.com>
2017-09-24 14:35:19 -0700

    US13 - Added routes for stub views

--- app/views.py
+++ app/views.py
@@ -5,3 +5,15 @@ from flask import render_template
 @app.route('/index', methods=['GET'])
 def indexView():
     return render_template('index.html', title='Home')
+
+@app.route('/bands', methods=['GET'])
+def bandsView():
+    return render_template('bands.html', title='Bands')
+
+@app.route('/dorms', methods=['GET'])
+def dormsView():
+    return render_template('dorms.html', title='Dorms')
+
+@app.route('/members', methods=['GET'])
+def membersView():
+    return render_template('members.html', title='Members')
