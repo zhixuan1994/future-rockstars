ff81a71e3718e5b99d3e3a955b4b81d21f7fcbce
Sean Collins <sean.collins@outlook.com>
2017-09-24 14:18:00 -0700

    US12 - Updated index route to render the new templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, world!"
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def indexView():
+    return render_template('index.html', title='Home')
