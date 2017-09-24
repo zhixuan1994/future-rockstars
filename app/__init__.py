508f7c15e4e03e47f800b50ea3a23e0762764522
Sean Collins <sean.collins@outlook.com>
2017-09-24 14:17:17 -0700

    US12 - Defined static and template directories

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
 
+CUR_DIR = os.path.realpath(os.path.dirname(__file__))
+app = Flask(
+    __name__,
+    static_folder=os.path.join(CUR_DIR, 'static'),
+    template_folder=os.path.join(CUR_DIR, 'templates'))
+app.config.update(
+    DEBUG=True
+)
+
 app = Flask(__name__)
+
 from app import views
