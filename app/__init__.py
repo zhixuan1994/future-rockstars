d0db1f873d985ff4b809f38b049f4c79d93450aa
Sean Collins <sean.collins@outlook.com>
2017-09-27 17:14:33 -0700

    US26 - Setup database to load fresh mocked data on refresh

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
+import json
+import os
 from flask import Flask
+from tinydb import TinyDB, Query
 
 CUR_DIR = os.path.realpath(os.path.dirname(__file__))
 app = Flask(
@@ -8,6 +11,19 @@ app = Flask(
 app.config.update(
     DEBUG=True
 )
+
+# Initialize the database with boilerplate JSON
+db = TinyDB('../db.json')
+db.purge()
+with open('./data/bands.json') as bands_data:
+    bands = json.load(bands_data)
+    db.insert_multiple(bands)
+with open('./data/members.json') as members_data:
+    members = json.load(members_data)
+    db.insert_multiple(members)
+with open('./data/dorms.json') as dorms_data:
+    dorms = json.load(dorms_data)
+    db.insert_multiple(dorms)
 
 app = Flask(__name__)
 
